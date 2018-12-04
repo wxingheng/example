6a8dc1e49fcf6a360596855e661c9c734688d925
wuxingheng <wuxh@wuxinghengdeMacBook-Pro.local>
2018-12-04 22:33:10 +0800

    egg

--- vue/shenzheng-vux/src/config/config.js
+++ vue/shenzheng-vux/src/config/config.js
@@ -3,5 +3,4 @@ export default {
   // const isDev = process.env.NODE_ENV === 'development'
   baseUrl: process.env.NODE_ENV === 'development' ? 'https://sbc.stpass.com' : location.origin
   // baseUrl: process.env.NODE_ENV === 'development' ? location.origin : location.origin
-
 }
