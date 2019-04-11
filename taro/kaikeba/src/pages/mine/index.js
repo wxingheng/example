15d945488a0c758ec9fdb8d352132fc0e9cb514b
wuxingheng <wuxh@wuxinghengdeMacBook-Pro.local>
2019-04-11 18:26:03 +0800

    fix

--- taro/kaikeba/src/pages/mine/index.js
+++ taro/kaikeba/src/pages/mine/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable jsx-quotes */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/first */
@@ -9,26 +10,40 @@ import defaultValues from "@/config/default-value.js";
 console.log("defaultValues", defaultValues);
 
 export default class Mine extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { userInfo: {} };
+  }
+
   config = {
     navigationBarTitleText: "我的"
   };
 
   login(userInfo) {
     console.log("login--->>>>>", userInfo);
-    Taro.login().then(loginRes => {
-      console.log("loginRes---->>>", loginRes);
-    });
+    if (typeof userInfo.target.userInfo === "undefined") {
+      return;
+    } else {
+      Taro.login().then(loginRes => {
+        console.log("loginRes---->>>", loginRes);
+      });
+      this.setState({
+        userInfo: userInfo.target.userInfo
+      });
+    }
   }
 
   render() {
-    const cover = defaultValues.mineCover;
+    const {userInfo} = this.state
+    const cover = userInfo.avatarUrl || defaultValues.mineCover
+    const name = userInfo.nickName || '点击登录'
     return (
       <View className="mine">
         <View className="mine-top">
           <Button className="mine-top-cover" openType="getUserInfo" onGetUserInfo={this.login}>
             <Image className="mine-top-cover-img" src={cover} />
           </Button>
-          <Text className="mine-top-name">Wuxh</Text>
+          <Text className="mine-top-name">{name}</Text>
         </View>
       </View>
     );
