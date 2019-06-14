e108619c50375a0dc7ab743a18c126a1f6e61b0f
wuxingheng <wuxh@wuxinghengdeMacBook-Pro.local>
2019-06-14 15:42:33 +0800

    fix

---
+++ RN/AwesomeProjectRN/App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    }
+    return (
+      <View style={styles.box}>
+        <Text>Hello world 001!</Text>
+        <Image source={pic} style={{width: '100%', height: 110, marginTop: 20}}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  box: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
