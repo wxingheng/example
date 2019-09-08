9923c9b87a1c8cfae25d103b6b797787aca0f3cf
wuxingheng <wuxh@wuxinghengdeMacBook-Pro.local>
2019-09-08 09:44:22 +0800

    feat: python

--- python/hello.py
+++ python/hello.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import urllib2
-import bs4
 
 url = "http://www.baidu.com"
-
 print '第一种方法'
 response1 = urllib2.urlopen(url)
 print response1.getcode()
 # print response1.read()
-print bs4
